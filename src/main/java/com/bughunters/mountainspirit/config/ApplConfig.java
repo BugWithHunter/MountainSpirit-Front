a9a0e0ab641bf49c64f7566b337cb941fe3482d5
HyeonggyuKang <kin0204@naver.com>
2025-09-22 17:39:13 +0900

    프로젝í트 샘플
    1. ContextBound 템í 템플릿

---
+++ src/main/java/com/bughunters/mountainspirit/config/ApplConfig.java
@@ -0,0 +1,11 @@
+package com.bughunters.mountainspirit.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplConfig {
+    @Bean
+    public ModelMapper modelMapper() {return new ModelMapper();}
+}
