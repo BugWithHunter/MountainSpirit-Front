23aac46b0b8d659d64c57774eca8289c3e6fab4c
HyeonggyuKang <kin0204@naver.com>
2025-09-22 16:49:06 +0900

    first Commit

---
+++ src/main/java/com/bughunters/mountainspirit/MountainspiritApplication.java
@@ -0,0 +1,13 @@
+package com.bughunters.mountainspirit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountainspiritApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MountainspiritApplication.class, args);
+    }
+
+}
