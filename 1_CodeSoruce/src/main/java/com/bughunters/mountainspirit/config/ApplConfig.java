13816d9d0ce9bc34510c244df4e1fb7b6bb4482b
HyeonggyuKang <kin0204@naver.com>
2025-09-25 21:33:09 +0900

    [fix] ModelMapper idê 관련 컬럼 맵핑 문제로 컬럼명이 동일할때만 맵핑 하도록 설정 추가

--- 1_CodeSoruce/src/main/java/com/bughunters/mountainspirit/config/ApplConfig.java
+++ 1_CodeSoruce/src/main/java/com/bughunters/mountainspirit/config/ApplConfig.java
@@ -4,13 +4,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplConfig {
     @Bean
-    public ModelMapper modelMapper() {return new ModelMapper();}
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
 
     @Bean
     public ObjectMapper objectMapper() {
