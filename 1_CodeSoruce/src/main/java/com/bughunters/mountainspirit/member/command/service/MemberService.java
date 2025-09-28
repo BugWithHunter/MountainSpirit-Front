0d5fff1f776851cee9f6170e81e7be696f0b999c
hyeongKang <kin0204@naver.com>
2025-09-28 17:50:16 +0900

    [ã[feat]member 패키지 구조 upload위í해 커밋

--- 1_CodeSoruce/src/main/java/com/bughunters/mountainspirit/member/command/service/MemberService.java
+++ 1_CodeSoruce/src/main/java/com/bughunters/mountainspirit/member/command/service/MemberService.java
@@ -2,7 +2,10 @@ package com.bughunters.mountainspirit.member.command.service;
 
 import com.bughunters.mountainspirit.member.command.dto.RequestModifyStatusOfMemberDTO;
 import com.bughunters.mountainspirit.member.command.dto.ResponseStatusDTO;
+import com.bughunters.mountainspirit.member.command.entity.Member;
 
 public interface MemberService {
     ResponseStatusDTO modifyStatusAfterClimbMountian(RequestModifyStatusOfMemberDTO modifyStatusOfMemberDTO);
+
+    Member getTest(Long id);
 }
