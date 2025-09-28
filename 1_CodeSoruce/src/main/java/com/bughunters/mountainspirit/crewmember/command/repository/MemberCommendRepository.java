aaff76735a03e2e5ae265ffd56b65b56857c5968
Kjandgo <rptmffld0204@gmail.com>
2025-09-28 18:49:51 +0900

    refact: #56 member service에서 crewId insert 구현

--- 1_CodeSoruce/src/main/java/com/bughunters/mountainspirit/crewmember/command/repository/MemberCommendRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bughunters.mountainspirit.crewmember.command.repository;
-
-import com.bughunters.mountainspirit.crewmember.command.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberCommendRepository extends JpaRepository<Member,Long> {
-}
