b62e022aebda2081fb2b672d6e7a53f1b051c515
HyeonggyuKang <kin0204@naver.com>
2025-09-30 17:01:45 +0900

    fix: gateway 프로ì젝트 수정 사항 커밋

--- 1_CodeSoruce/src/main/java/com/bughunters/mountainspirit/report/command/infrastructure/MemberFeignClient.java
+++ 1_CodeSoruce/src/main/java/com/bughunters/mountainspirit/report/command/infrastructure/MemberFeignClient.java
@@ -3,20 +3,17 @@ package com.bughunters.mountainspirit.report.command.infrastructure;
 import com.bughunters.mountainspirit.report.command.dto.ReportMemberDTO;
 import com.bughunters.mountainspirit.report.command.dto.ReportMemberUpdateDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "member-client" , url = "localhost:8080/member")
+@FeignClient(name = "feignMemberFeignClient" , url = "localhost:8000/member-client")
 public interface MemberFeignClient {
 
     // 회원 조회
-    @GetMapping("/report/member-info/{id}")
+    @GetMapping("/member/report/member-info/{id}")
     ReportMemberDTO getMemberInfo(@PathVariable("id") Long id);
 
     // 회원 상태 업데이트 (banCnt, memStsId)
-    @PatchMapping("/report/update-status/{id}")
+    @PostMapping("/member/report/update-status/{id}")
     void updateMemberStatus(@PathVariable("id") Long id,
                             @RequestBody ReportMemberUpdateDTO dto);
 
