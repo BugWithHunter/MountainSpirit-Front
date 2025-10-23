7a9489ac4fe9570375e7b32c338ff53f2cbfd964
suhyun-h <suhyun8913@naver.com>
2025-10-23 16:19:26 +0900

    전체 산 페이징 처리

--- 1_CodeSoruce/src/main/java/com/bughunters/mountainspirit/mountain/query/service/MountainSearchService.java
+++ 1_CodeSoruce/src/main/java/com/bughunters/mountainspirit/mountain/query/service/MountainSearchService.java
@@ -2,6 +2,8 @@ package com.bughunters.mountainspirit.mountain.query.service;
 
 import com.bughunters.mountainspirit.mountain.query.dto.CourseDTO;
 import com.bughunters.mountainspirit.mountain.query.dto.MountainDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -13,4 +15,8 @@ public interface MountainSearchService {
     List<CourseDTO> getAllCourseInfo();
 
     List<CourseDTO> getSearchCourseInfo(String mountainName);
+
+    Page<MountainDTO> getAllMountainInfo(Pageable pageable);
+
+//    Page<CourseDTO> getAllCourseInfo(Pageable pageable);
 }
